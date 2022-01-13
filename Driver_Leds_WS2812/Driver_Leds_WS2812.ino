915037b4652a85e455c272a5fdbdadff1d12d3bd
Marc Sibert <marc@sibert.fr>
2022-01-13 16:04:49 +0100

    Update Driver_Leds_WS2812.ino

--- Driver_Leds_WS2812/Driver_Leds_WS2812.ino
+++ Driver_Leds_WS2812/Driver_Leds_WS2812.ino
@@ -16,23 +16,30 @@
 
 #include "application.h"
 
-static const byte NB_LED = 4;
+#if ARDUINO_AVR_UNO
+#define SERIAL true
+static const auto PIN_LEDS = 8;
+static const auto NB_LED = 4;
+static const auto PIN_LDR = A0;
+#endif
 
-auto app = Application<NB_LED>();
+auto app = Application<NB_LED, PIN_LEDS, PIN_LDR>();
 
 /**
  * Lancée une fois à l'initialisation.
  */
 void setup() {
   delay(1000);
-  Serial.begin(115200);
-  while (!Serial);
-  Serial.println(F(__FILE__ " compiled " __DATE__ " " __TIME__));
-  Serial.println(F("Copyright (c) 2021 par Marc SIBERT"));
-  Serial.println();
-
+  if (SERIAL) {
+    Serial.begin(115200);
+    while (!Serial);
+    Serial.println(F(__FILE__ " compiled " __DATE__ " " __TIME__));
+    Serial.println(F("Copyright (c) 2021 par Marc SIBERT"));
+    Serial.println();
+  }
+  
   if (!app.setup()) {
-    Serial.println("Erreur d'initialisation ! Arrêt !");
+    if (SERIAL) Serial.println("Erreur d'initialisation ! Arrêt !");
     exit(1);
   }
 }
@@ -42,7 +49,7 @@ void setup() {
  */
 void loop() {
   if (!app.loop()) {
-    Serial.println("Erreur d'exécution ! Arrêt !");
+    if (SERIAL) Serial.println("Erreur d'exécution ! Arrêt !");
     exit(1);
   }
 }
